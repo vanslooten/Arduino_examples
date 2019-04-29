7ee9b55f2cf4f1ac96cb89ca4afc394cfd3d9b35
Fjodor van Slooten <github@vanslooten.com>
2019-04-29 11:37:17 +0200

    Add files via upload

---
+++ LCD_HD44780_i2c_hello_world_example.ino
@@ -0,0 +1,50 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+#define BACKLIGHT_PIN     3
+
+LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7); // first parameter is address, can be 0x27, 0x3F or 0x38
+
+// Creat a set of new characters
+const uint8_t charBitmap[][8] = {
+   { 0xc, 0x12, 0x12, 0xc, 0, 0, 0, 0 },
+   { 0x6, 0x9, 0x9, 0x6, 0, 0, 0, 0 },
+   { 0x0, 0x6, 0x9, 0x9, 0x6, 0, 0, 0x0 },
+   { 0x0, 0xc, 0x12, 0x12, 0xc, 0, 0, 0x0 },
+   { 0x0, 0x0, 0xc, 0x12, 0x12, 0xc, 0, 0x0 },
+   { 0x0, 0x0, 0x6, 0x9, 0x9, 0x6, 0, 0x0 },
+   { 0x0, 0x0, 0x0, 0x6, 0x9, 0x9, 0x6, 0x0 },
+   { 0x0, 0x0, 0x0, 0xc, 0x12, 0x12, 0xc, 0x0 }
+};
+
+void setup() {
+  int charBitmapSize = (sizeof(charBitmap ) / sizeof (charBitmap[0]));
+
+  lcd.begin(16,2); // initialize the lcd
+  // Switch on the backlight:
+  lcd.setBacklightPin(3,POSITIVE);
+  lcd.setBacklight(HIGH); // turn on backlight
+  
+   for ( int i = 0; i < charBitmapSize; i++ ) {
+      lcd.createChar( i, (uint8_t *)charBitmap[i] );
+   }
+
+  lcd.home(); // go to start position
+  lcd.print("Hello, world!");  
+  lcd.setCursor(0,1);        // go to the next line
+  lcd.print("AppDev example");
+  delay (3000);
+}
+
+void loop() {
+   lcd.home ();
+   // Do a little animation by writing to the same location
+   for ( int i = 0; i < 2; i++ ) {
+      for ( int j = 0; j < 16; j++ ) {
+         lcd.print(char(random(7)));
+      }
+      lcd.setCursor( 0, 1 );
+   }
+   delay(200);
+}
+
